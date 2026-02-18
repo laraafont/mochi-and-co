b1fe1073021a35adc81a69d00f1ed0a7409a82fb
Lara Afont <laraafont@gmail.com>
2026-02-18 15:37:05 -0500

    updated file structure, creating styling sheets, setting up component files, implementing basic page routing

---
+++ theme/colors.ts
@@ -0,0 +1,7 @@
+export const colors = {
+  background: "#D5B893",
+  primary: "#6F4D38",
+
+  textPrimary: "#F8ECDD",
+  textSecondary: "#DFB988",
+};
---
+++ theme/typography.ts
@@ -0,0 +1,11 @@
+export const fonts = {
+  regular: "Kodchasan_400Regular",
+  semiBold: "Kodchasan_600SemiBold",
+  bold: "Kodchasan_700Bold",
+};
+
+export const fontSizes = {
+  xl: 40,
+  md: 24,
+  xs: 16,
+};
